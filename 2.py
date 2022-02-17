9bd061f4114497a879cfecd676a458c56f617c97
Shakil <nshosain@gmail.com>
2022-02-17 15:54:18 +0600

    fibo calling done. logic set.

--- 2.py
+++ 2.py
@@ -1,18 +1,15 @@
-# Python program to display the Fibonacci sequence
-
-def recur_fibo(n):
+def recur_fibo(n):  # returns fibonacci series
     if n <= 1:
         return n
     else:
         return recur_fibo(n - 1) + recur_fibo(n - 2)
 
 
-nterms = 10
-
-# check if the number of terms is valid
-if nterms <= 0:
-    print("Plese enter a positive integer")
-else:
-    print("Fibonacci sequence:")
-    for i in range(nterms):
-        print(recur_fibo(i))
+i = 0  # iterator
+while True:  # an infinite loop
+    last_number = recur_fibo(i)  # stores last digit of the series
+    if last_number > 50:  # checks if last number of series is less than 4 million
+        break  # breaks loop if reached
+    else:  # runs loop if last number < 4 million
+        print(last_number)  # prints last number
+        i += 1  # increases iterator value
