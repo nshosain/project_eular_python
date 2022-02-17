edf9c58d771d77897de1a1192c9e338278da1efb
Takin <fahim.tanzil@hotmail.com>
2022-02-18 00:55:28 +0600

    Problem #2 Solved

--- 2.py
+++ 2.py
@@ -6,10 +6,16 @@ def recur_fibo(n):  # returns fibonacci series
 
 
 i = 0  # iterator
+fibosum = 0
+
 while True:  # an infinite loop
     last_number = recur_fibo(i)  # stores last digit of the series
-    if last_number > 50:  # checks if last number of series is less than 4 million
-        break  # breaks loop if reached
-    else:  # runs loop if last number < 4 million
-        print(last_number)  # prints last number
+    if last_number <= 4000000:  # runs loop if last number < 4 million
+        # print(last_number) # prints the last number everytime
+        if last_number % 2 == 0:  # Checks if the number is divisible by 2
+            fibosum += last_number  # Adds latest number to sum
         i += 1  # increases iterator value
+    else:  # checks if last number of series is less than 4 million
+        break  # breaks loop if reached
+
+print(fibosum)
